5f1bc39d51815b702f88bb0f322ec52c9a3b5604
zhengwenbo <19802021891@139.com>
2022-08-03 15:11:52 +0800

    完成登录表单样式

--- src/components/ShopLogin.vue
+++ src/components/ShopLogin.vue
@@ -1,5 +1,23 @@
 <template>
-<h1>登录组件</h1>
+  <div class="login_container">
+    <div class="login_box">
+      <div class="avatar_box">
+        <img src="../assets/logo.png">
+      </div>
+      <el-form class="login_form">
+        <el-form-item>
+          <el-input prefix-icon="iconfont icon-user"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input prefix-icon="iconfont icon-3702mima"></el-input>
+        </el-form-item>
+        <el-form-item class="login_btns">
+          <el-button type="primary">登录</el-button>
+          <el-button type="info">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +26,53 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
 
+.login_box {
+  background-color: white;
+  width: 450px;
+  height: 300px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  border-radius: 5px;
+  transform: translate(-50%, -50%);
+
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    background-color: white;
+    position: absolute;
+    left: 50%;
+    border: 1px solid #eee;
+    box-shadow: 0 0 10px #ddd;
+    border-radius: 50%;
+    padding: 10px;
+    transform: translate(-50%, -50%);
+
+    img {
+      height: 100%;
+      width: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+}
+
+.login_form {
+  padding: 0 20px;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  box-sizing: border-box;
+}
+
+.login_btns {
+  display: flex;
+  justify-content: end;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+//引入路由
 import VueRouter from "vue-router";
 import router from './router'
+//引入全局样式
+import './assets/css/global.css'
+//引入element ui组件库
+import {Button, Form, Input, FormItem} from "element-ui";
+//引入图标库
+import './assets/fonts/iconfont.css'
 
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
 
+Vue.component(Button.name, Button)
+Vue.component(Form.name, Form)
+Vue.component(Input.name, Input)
+Vue.component(FormItem.name, FormItem)
+
+/*Vue.use(Button)
+Vue.use(Form)*/
+
 new Vue({
-  render: h => h(App),
-  router
+    render: h => h(App),
+    router
 }).$mount('#app')
