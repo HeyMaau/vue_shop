6e31f2f0fdd42c4816c5db5a69b8887f0b429d63
zhengwenbo <19802021891@139.com>
2022-08-22 15:47:39 +0800

    完成订单管理-订单列表渲染

--- src/components/order/ShopOrder.vue
+++ src/components/order/ShopOrder.vue
@@ -8,6 +8,7 @@
     </el-breadcrumb>
     <!--  卡片区域  -->
     <el-card class="box-card">
+      <!--   搜索框   -->
       <el-row>
         <el-col :span="8">
           <el-input placeholder="请输入内容">
@@ -15,13 +16,80 @@
           </el-input>
         </el-col>
       </el-row>
+      <!--   表格区域   -->
+      <el-table
+          :data="orderList"
+          style="width: 100%">
+        <!--    索引列    -->
+        <el-table-column type="index" label="#"></el-table-column>
+        <el-table-column
+            prop="order_number"
+            label="订单编号">
+        </el-table-column>
+        <el-table-column
+            prop="order_price"
+            label="订单价格">
+        </el-table-column>
+        <el-table-column
+            label="是否付款">
+          <template slot-scope="scope">
+            <el-tag type="success" v-if="scope.row.pay_status === '1'">已付款</el-tag>
+            <el-tag type="danger" v-else>未付款</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column
+            prop="is_send"
+            label="是否发货">
+        </el-table-column>
+        <el-table-column
+            label="下单时间">
+          <template slot-scope="scope">
+            {{ scope.row.create_time | dataFormater }}}
+          </template>
+        </el-table-column>
+        <el-table-column
+            label="操作">
+          <template>
+            <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+            <el-button type="success" icon="el-icon-location" size="mini"></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: "ShopOrder"
+  name: "ShopOrder",
+  created() {
+    this.getOrderList()
+  },
+  data() {
+    return {
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 10
+      },
+      orderList: [],
+      total: 0
+    }
+  },
+  methods: {
+    async getOrderList() {
+      const {data: response} = await this.$http.get('orders', {
+        params: this.queryInfo
+      })
+      console.log(response)
+      if (response.meta.status !== 200) {
+        this.$message.error('获取订单数据失败！')
+      } else {
+        this.orderList = response.data.goods
+        this.total = response.data.total
+      }
+    }
+  }
 }
 </script>
 
