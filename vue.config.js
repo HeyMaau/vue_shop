684f3bd1b1e8b991d4a50985bbdbd2e638d1d32e
zhengwenbo <19802021891@139.com>
2022-08-24 15:54:04 +0800

    生产环境改用CDN引入，首页结构区分开发环境与生产环境

--- vue.config.js
+++ vue.config.js
@@ -3,10 +3,26 @@ module.exports = {
         // 为生产环境修改配置...
         config.when(process.env.NODE_ENV === 'production', config => {
             config.entry('app').clear().add('./src/main-prod.js')
+            config.set('externals', {
+                vue: 'Vue',
+                axios: 'axios',
+                lodash: '_',
+                echarts: 'echarts',
+                nprogress: 'NProgress',
+                'vue-quill-editor': 'VueQuillEditor'
+            })
+            config.plugin('html').tap(args => {
+                args[0].isProd = true
+                return args
+            })
         })
         // 为开发环境修改配置...
         config.when(process.env.NODE_ENV === 'development', config => {
             config.entry('app').clear().add('./src/main-dev.js')
+            config.plugin('html').tap(args => {
+                args[0].isProd = false
+                return args
+            })
         })
     },
     transpileDependencies: true
